b56abeeb1f70c39fa2a8960e402e3ef3f57a1227
Declan Brady <drbrady8800@gmail.com>
2025-01-17 18:50:23 -0500

    Fallback to latin-1 if utf-8 fails

--- textract/parsers/txt_parser.py
+++ textract/parsers/txt_parser.py
@@ -1,9 +1,14 @@
 from .utils import BaseParser
 
-
 class Parser(BaseParser):
     """Parse ``.txt`` files"""
 
     def extract(self, filename, **kwargs):
-        with open(filename) as stream:
-            return stream.read()
+        try:
+            # Try UTF-8 first
+            with open(filename, encoding='utf-8') as stream:
+                return stream.read()
+        except UnicodeDecodeError:
+            # Fallback to latin-1 which will never fail
+            with open(filename, encoding='latin-1') as stream:
+                return stream.read()
